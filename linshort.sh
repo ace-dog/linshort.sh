888e38f1905d790c12050dce36e104659d37eb24
adil <adil@bird-dog.tv>
2023-11-27 20:27:08 +1100

    feat[ps1] change terminal color

--- linshort.sh
+++ linshort.sh
@@ -8,6 +8,6 @@ git_fold() {
     fi
     echo $gitfold
 }
-PS1='${debian_chroot:+($debian_chroot)}\[\033[03;34m\]<\W\[\033[04;01;35m\]$(git_fold)\[\033[00;34m\]>\[\033[00;37m\] '
+PS1='${debian_chroot:+($debian_chroot)}\033[1;3;34;46m\W\033[04;01;35;105m$(git_fold)\033[00;91;49m>\033[00;37;49m'
 source $1/gitshort.sh
 source $1/sshshort.sh
