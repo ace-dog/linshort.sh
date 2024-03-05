4a50980660a2eaa600cf0a072d4ab5e58c9216d6
ace-dog <adil@bird-dog.tv>
2024-03-05 12:27:46 +1100

    feat[add alias] add lsh, fdfind, batcat and autojump

--- linshort.sh
+++ linshort.sh
@@ -9,5 +9,12 @@ git_fold() {
     echo $gitfold
 }
 PS1='${debian_chroot:+($debian_chroot)}\[\033[03;34m\]<\W\[\033[04;01;35m\]$(git_fold)\[\033[00;34m\]>\[\033[00;37m\] '
+
+alias ls='lsd'
+alias ll='lsd -l'
+alias lst='lsd --tree'
+alias fdf='fdfind'
+alias bat='batcat'
+. /usr/share/autojump/autojump.sh
 source $1/gitshort.sh
 source $1/sshshort.sh
