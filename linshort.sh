14cb3e3eb738690a7c9286d260e9f45f262caeb3
ace-dog <adil@bird-dog.tv>
2024-03-06 13:53:36 +1100

    feat[PS1] only run on change of dir

--- linshort.sh
+++ linshort.sh
@@ -1,6 +1,6 @@
 linshortpath=$1
 export PS1GITFOLD=$(pwd)
-git_fold() {
+git_fold() { 
     gitfold=
     if [ -d ".git" ]; then
         gitbranch=$(git branch 2>/dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
@@ -10,6 +10,13 @@ git_fold() {
     echo $gitfold
 }
 PS1='${debian_chroot:+($debian_chroot)}\[\033[03;34m\]<\W\[\033[04;01;35m\]$(git_fold)\[\033[00;34m\]>\[\033[00;37m\] '
+custom_prompt() {
+    if [ "$PS1GITFOLD" != "$PWD" ]; then
+        PS1='${debian_chroot:+($debian_chroot)}\[\033[03;34m\]<\W\[\033[04;01;35m\]$(git_fold)\[\033[00;34m\]>\[\033[00;37m\] ' 
+    fi    
+    PS1GITFOLD=$(pwd)
+}
+PROMPT_COMMAND=custom_prompt
 if ! command -v lsd  &> /dev/null
 then
     echo "installing lsd"
