9289cd795f4908b7fc9e6ff0b19e676382efc0f8
ace-dog <adilarshad8901@gmail.com>
2024-08-22 16:23:23 +1000

    refactor[packages] get deb packages direclty

--- linshort.sh
+++ linshort.sh
@@ -20,29 +20,31 @@ PROMPT_COMMAND=custom_prompt
 if ! command -v lsd  &> /dev/null
 then
     echo "installing lsd"
-    sudo snap  install lsd
+    wget https://github.com/lsd-rs/lsd/releases/download/v1.1.5/lsd-musl_1.1.5_amd64.deb --output-document=/tmp/lsd.deb
+    sudo dpkg -i /tmp/lsd.deb
 fi
 alias ls='lsd'
 alias ll='lsd -l'
 alias lst='lsd --tree'
 export PATH="$HOME/.cargo/bin:$PATH"
-if ! command -v fdfind  &> /dev/null
+if ! command -v fd  &> /dev/null
 then
     echo "installing fd-find"
-    sudo apt install fd-find
+    wget https://github.com/sharkdp/fd/releases/download/v10.1.0/fd-musl_10.1.0_amd64.deb --output-document=/tmp/fd.deb
+    sudo dpkg -i /tmp/fd.deb
 fi
-alias fdf='fdfind'
-if ! command -v batcat  &> /dev/null
+if ! command -v bat  &> /dev/null
 then
     echo "installing bat"
-    sudo apt install bat
+    wget https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-musl_0.24.0_amd64.deb --output-document=/tmp/bat.deb
+    sudo dpkg -i /tmp/bat.deb
 fi
-alias bat='batcat'
-if ! command -v autojump  &> /dev/null
-then
-    echo "installing autojump"
-    sudo apt install autojump
-fi
-. /usr/share/autojump/autojump.sh
+
+alias cat='bat'
+
+# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.tar.xz --output-document=/tmp/fonts.tar.xz
+# mkdir -p ~/.fonts
+# tar -xf /tmp/fonts.tar.xz -C ~/.fonts
+
 source $1/gitshort.sh
 source $1/dockershort.sh
